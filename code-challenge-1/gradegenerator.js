9f87c3aff3a21fc5bfb899b13aa588e519267a4f
Nick-bee679 <mwauranicholas10@gmail.com>
2022-11-28 02:56:13 +0300

    finished gradegenerator.js

---
+++ code-challenge-1/gradegenerator.js
@@ -0,0 +1,42 @@
+function studentGrade(grade) 
+//this fuction uses if else to find  the grades
+	{
+	if (grade <= 100) {
+
+
+		if (grade > 79) {
+			console.log(`The grade is A`);
+		}
+		//if the input is greater than 79 
+
+		else if (grade >= 60) {
+			console.log(`The grade is B`);
+		} 
+		//if the input is greater than 60
+		
+		else if (grade > 49) {
+			console.log(`The grade is C`);
+		} 
+		//if the input is greater than 49
+		
+		else if (grade >= 40) {
+			console.log(`The grade is D`);
+		} 
+		//if the  input is greater than 40
+		
+		else if (grade < 40) {
+			console.log(`The grade is E`);
+		}
+		//if the input is less than 40
+		
+		else{
+			return "invalid!"
+		}
+		//if input is not a number or is a string
+    }
+}
+	
+
+studentGrade (10)
+
+
